2a0a36afc9368f799bc628bc9904dfaac82ce0db
lngg21 <arthur.vinigamer@gmail.com>
2026-08-24 15:53:13 -0300

    first commit: Estrutura inicial do projeto

---
+++ main.tf
@@ -0,0 +1,22 @@
+providers aws {
+}
+
+
+locals {
+  l = value
+}
+
+resource aws{}
+resource aws{}
+resource aws{}
+resource aws{}
+resource aws{}
+
+output{
+  value       = ""
+  sensitive   = true
+  description = "description"
+  depends_on  = []
+}
+
+
